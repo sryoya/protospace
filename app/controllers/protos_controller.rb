35a556d82e644051fe4e27c7a0aee5f90e1b1edb
関野 涼也 <sryoya0814@gmail.com>
2015-10-16 04:29:06 +0900

    prototypeの保存機能も追加

--- app/controllers/protos_controller.rb
+++ app/controllers/protos_controller.rb
@@ -4,8 +4,12 @@ class ProtosController < ApplicationController
   end
 
   def create
+    Proto.create(title: create_params[:title], catch_copy: create_params[:catch_copy], concept: create_params[:concept])
     redirect_to :root and return
   end
 
-
+  private
+    def create_params
+      params.require(:proto).permit(:title, :catch_copy, :concept)
+    end
 end
