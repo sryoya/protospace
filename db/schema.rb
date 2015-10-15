95f8cb0cfe4ed876932ac882f6af8662defff0e1
関野 涼也 <sryoya0814@gmail.com>
2015-10-16 02:11:22 +0900

    carrierwaveの導入とavatarの導入

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012061926) do
+ActiveRecord::Schema.define(version: 20151015115219) do
+
+  create_table "protos", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "catch_copy", limit: 255
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -30,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151012061926) do
     t.string   "works",                  limit: 255
     t.string   "group",                  limit: 255
     t.text     "profile",                limit: 65535
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
