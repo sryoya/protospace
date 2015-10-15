9d574b0136468956634a980d65affe73c594244e
関野 涼也 <sryoya0814@gmail.com>
2015-10-16 06:24:59 +0900

    ユーザー登録のバリデーションの設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
