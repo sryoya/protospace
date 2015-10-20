73f9bb378930e969b01f25ff38392d8d82e50bbb
関野 涼也 <sryoya0814@gmail.com>
2015-10-20 22:35:18 +0900

    protoコントローラを編集してproto_imageテーブルに保存できるように

--- app/controllers/protos_controller.rb
+++ app/controllers/protos_controller.rb
@@ -4,12 +4,22 @@ class ProtosController < ApplicationController
   end
 
   def create
-    Proto.create(title: create_params[:title], catch_copy: create_params[:catch_copy], concept: create_params[:concept])
+    binding.pry
+    proto = Proto.create(title: create_params[:title], catch_copy: create_params[:catch_copy], concept: create_params[:concept])
+
+    ProtoImage.create(image: create_params[:main_image], status: "main", proto_id: proto.id)
+
+    if create_params[:sub_image1].present?
+      ProtoImage.create(image: create_params[:sub_image1], status: "sub", proto_id: proto.id)
+    end
+    if create_params[:sub_image2].present?
+      ProtoImage.create(image: create_params[:sub_image2], status: "sub", proto_id: proto.id)
+    end
     redirect_to :root and return
   end
 
   private
     def create_params
-      params.require(:proto).permit(:title, :catch_copy, :concept)
+      params.require(:proto).permit(:title, :catch_copy, :concept, :main_image, :sub_image1, :sub_image2)
     end
 end
