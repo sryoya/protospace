a29dff7d0a56dff42d475fcc10356cc4b39f7f16
関野 涼也 <sryoya0814@gmail.com>
2015-10-23 23:50:24 +0900

    proto_imageがないときにprotoが保存されないようにvalidationを設定

--- app/models/proto.rb
+++ app/models/proto.rb
@@ -2,6 +2,7 @@ class Proto < ActiveRecord::Base
   belongs_to :user
   has_many :proto_images
   accepts_nested_attributes_for :proto_images, allow_destroy: true, reject_if: :reject_proto_images
+  validates :proto_images, presence: true
   attr_accessor :tag1, :tag2, :tag3
 
   def reject_proto_images(attributed)
