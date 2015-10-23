568cb65f23cce869540ae13a08551c15d99f5b13
関野 涼也 <sryoya0814@gmail.com>
2015-10-23 21:07:15 +0900

    accepts_nested_attributes_forとfields_forでphotoの作成と同時にproto_imageの保存ができるようにした。reject_ifをつけてimageが空のproto_imageが保存されないようにした。

--- app/controllers/protos_controller.rb
+++ app/controllers/protos_controller.rb
@@ -1,24 +1,17 @@
 class ProtosController < ApplicationController
   def new
     @proto = Proto.new
+    @proto.proto_images.build
   end
 
   def create
-    proto = Proto.create(title: create_params[:title], catch_copy: create_params[:catch_copy], concept: create_params[:concept])
-
-    ProtoImage.create(image: create_params[:main_image], status: "main", proto_id: proto.id)
-
-    if create_params[:sub_image1].present?
-      ProtoImage.create(image: create_params[:sub_image1], status: "sub", proto_id: proto.id)
-    end
-    if create_params[:sub_image2].present?
-      ProtoImage.create(image: create_params[:sub_image2], status: "sub", proto_id: proto.id)
-    end
+    Proto.new(create_params)
+    
     redirect_to :root and return
   end
 
   private
     def create_params
-      params.require(:proto).permit(:title, :catch_copy, :concept, :main_image, :sub_image1, :sub_image2)
+      params.require(:proto).permit(:title, :catch_copy, :concept, proto_images_attributes: [:image, :status])
     end
 end
--- app/models/proto.rb
+++ app/models/proto.rb
@@ -1,5 +1,10 @@
 class Proto < ActiveRecord::Base
   belongs_to :user
-  has_many :images
+  has_many :proto_images
+  accepts_nested_attributes_for :proto_images, allow_destroy: true, reject_if: :reject_proto_images
   attr_accessor :tag1, :tag2, :tag3
+
+  def reject_proto_images(attributed)
+    attributed['image'].blank?
+  end
 end
