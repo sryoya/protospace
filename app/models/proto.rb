2a4ceb1aeba0089f50c5012c44be2a38c8a6bf47
関野 涼也 <sryoya0814@gmail.com>
2015-10-24 17:55:18 +0900

    バリデーションの記載を短く

--- app/models/proto.rb
+++ app/models/proto.rb
@@ -1,9 +1,7 @@
 class Proto < ActiveRecord::Base
   belongs_to :user
   has_many :proto_images
-  validates :title, presence: true
-  validates :catch_copy, presence: true
-  validates :concept, presence: true
+  validates :title, :catch_copy, :concept, presence: true
   accepts_nested_attributes_for :proto_images, allow_destroy: true, reject_if: :reject_proto_images
   validates :proto_images, presence: true
   attr_accessor :tag1, :tag2, :tag3
