bb3e5c208a281fe1e6c6876a1882bbd687214229
関野 涼也 <sryoya0814@gmail.com>
2015-10-30 03:47:45 +0900

    proto_imageテーブルのカラムをintegerにしてenumを導入

---
+++ db/migrate/20151029184354_change_status_to_proto_images.rb
@@ -0,0 +1,9 @@
+class ChangeStatusToProtoImages < ActiveRecord::Migration
+  def up
+    change_column :proto_images, :status, :integer, null: false, default: 1
+  end
+
+  def down
+    change_column :proto_images, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024094142) do
+ActiveRecord::Schema.define(version: 20151029184354) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "proto_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+  end
+
+  add_index "comments", ["proto_id"], name: "index_comments_on_proto_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "proto_images", force: :cascade do |t|
     t.text     "image",      limit: 65535
-    t.string   "status",     limit: 255
+    t.integer  "status",     limit: 4,     default: 1, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "proto_id",   limit: 4
@@ -77,5 +88,7 @@ ActiveRecord::Schema.define(version: 20151024094142) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "protos"
+  add_foreign_key "comments", "users"
   add_foreign_key "proto_images", "protos"
 end
