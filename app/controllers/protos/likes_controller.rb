308d7b0c64831517079043f85ebeb574a52ae4c1
関野 涼也 <sryoya0814@gmail.com>
2015-11-04 07:42:22 +0900

    同期通信でlikeをする・外すができるようにした

--- app/controllers/protos/likes_controller.rb
+++ app/controllers/protos/likes_controller.rb
@@ -1,4 +1,21 @@
-class LikesController < ApplicationController
+class Protos::LikesController < ApplicationController
   def create
+    Like.create(create_params)
+    proto = Proto.find(params[:like][:proto_id])
+    @likes = proto.likes
+    redirect_to proto_path(params[:like][:proto_id]) and return
+  end
+
+  def destroy
+    like = Like.find(params[:id])
+    if like.user_id == current_user.id
+      like.destroy
+    end
+  redirect_to proto_path(like.proto_id) and return
+  end
+
+  private
+  def create_params
+    params.require(:like).permit(:proto_id).merge(user_id: current_user.id)
   end
 end
