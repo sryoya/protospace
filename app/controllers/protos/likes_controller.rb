3069b01587fabccb77e4d81bfb6429d9c6b8406c
関野 涼也 <sryoya0814@gmail.com>
2015-11-06 02:30:53 +0900

    同じユーザーが二回いいねできないように設定

--- app/controllers/protos/likes_controller.rb
+++ app/controllers/protos/likes_controller.rb
@@ -1,8 +1,11 @@
 class Protos::LikesController < ApplicationController
   def create
+  unless Like.find_by(user_id: current_user.id)
     Like.create(create_params)
+  end
     @proto = Proto.find(params[:like][:proto_id])
     @likes = @proto.likes
+    @like = Like.new
   end
 
   def destroy
