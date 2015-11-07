35614b3ef392da99d062040de5b2e4fb26880b23
関野 涼也 <sryoya0814@gmail.com>
2015-11-08 07:48:53 +0900

    PR修正　likeコントローラー内の記述の最適化

--- app/controllers/protos/likes_controller.rb
+++ app/controllers/protos/likes_controller.rb
@@ -1,25 +1,19 @@
 class Protos::LikesController < ApplicationController
   def create
-  unless Like.find_by(user_id: current_user.id)
-    Like.create(create_params)
-  end
+    Like.where(user_id: current_user.id, proto_id: params[:proto_id]).first_or_create
     @proto = Proto.find(params[:proto_id])
     @likes = Like.where(proto_id: params[:proto_id])
     @like = Like.where(proto_id: params[:proto_id], user_id: current_user.id)
   end
 
   def destroy
-  like = Like.find(params[:id])
-  if like.user_id == current_user.id
-    like.destroy
-  end
-  @proto = Proto.find(params[:proto_id])
-  @likes = Like.where(proto_id: params[:proto_id])
-  @like = Like.where(proto_id: params[:proto_id], user_id: current_user.id)
+    like = Like.find(params[:id])
+    if like.user_id == current_user.id
+      like.destroy
+    end
+    @proto = Proto.find(params[:proto_id])
+    @likes = Like.where(proto_id: params[:proto_id])
+    @like = Like.where(proto_id: params[:proto_id], user_id: current_user.id)
   end
 
-  private
-  def create_params
-    params.permit(:proto_id).merge(user_id: current_user.id)
-  end
 end
