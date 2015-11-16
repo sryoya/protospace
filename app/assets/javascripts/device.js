06a1fc690a585ada5b62986e69282503ff7f3f62
関野 涼也 <sryoya0814@gmail.com>
2015-11-17 07:42:47 +0900

    PR修正　変数の定義をvarからletに書き換え　インデントも正しく行う

--- app/assets/javascripts/device.js
+++ app/assets/javascripts/device.js
@@ -1,12 +1,12 @@
 $(function(){
   $("#user_avatar").change(function(){
-        var uploaded_avatar = $("#uploaded_avatar");
-        var file = $(this).prop('files')[0];
-        var fr = new FileReader();
-        fr.onload = function(){
-        uploaded_avatar.removeAttr('src')
-        uploaded_avatar.attr('src', fr.result );
-        }
-      fr.readAsDataURL(file);
-    });
+    let uploaded_avatar = $("#uploaded_avatar");
+    let file = $(this).prop('files')[0];
+    let fr = new FileReader();
+    fr.onload = function(){
+      uploaded_avatar.removeAttr('src')
+      uploaded_avatar.attr('src', fr.result );
+    };
+    fr.readAsDataURL(file);
+  });
 });
--- app/assets/javascripts/proto.js
+++ app/assets/javascripts/proto.js
@@ -1,22 +1,22 @@
 $(function(){
 
-for(var i = 0; i < 4; i++){
-  $("#proto_proto_images_attributes_" + i + "_image").change(function(){
-      var num = $(this).attr("id").split("_")[4];
-      var uploaded_image = $("#uploaded-image-" + num);
-      var file = $(this).prop('files')[0];
-      var fr = new FileReader();
+  for(let i = 0; i < 4; i++){
+    $("#proto_proto_images_attributes_" + i + "_image").change(function(){
+      let num = $(this).attr("id").split("_")[4];
+      let uploaded_image = $("#uploaded-image-" + num);
+      let file = $(this).prop('files')[0];
+      let fr = new FileReader();
       fr.onload = function(){
-      uploaded_image.attr('src', fr.result );
-      uploaded_image.css({'height':'100%', 'width': '100%'});
-      }
-    fr.readAsDataURL(file);
-  });
+        uploaded_image.attr('src', fr.result );
+        uploaded_image.css({'height':'100%', 'width': '100%'});
+      };
+      fr.readAsDataURL(file);
+    });
   };
 
-$(".image-upload-plus").click(function(){
-$(this).hide();
-$("#another-form").removeAttr('id');
-});
+  $(".image-upload-plus").click(function(){
+    $(this).hide();
+    $("#another-form").removeAttr('id');
+  });
 
 });
