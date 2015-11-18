4813014b09dd999ad7fb78ee5b9b4ae2ecc1d513
関野 涼也 <sryoya0814@gmail.com>
2015-11-18 19:58:41 +0900

    コントローラー内で共通した記述をメソッド化

--- app/controllers/protos/likes_controller.rb
+++ app/controllers/protos/likes_controller.rb
@@ -1,14 +1,17 @@
 class Protos::LikesController < ApplicationController
   def create
     Like.where(user_id: current_user.id, proto_id: params[:proto_id]).first_or_create if user_signed_in?
-    @proto = Proto.find(params[:proto_id])
-    @likes = Like.where(proto_id: params[:proto_id])
-    @like = Like.where(proto_id: params[:proto_id], user_id: current_user.id)
+    get_proto_and_likes
   end
 
   def destroy
     like = Like.find(params[:id])
     like.destroy if like.user_id == current_user.id
+    get_proto_and_likes
+  end
+
+  private
+  def get_proto_and_likes
     @proto = Proto.find(params[:proto_id])
     @likes = Like.where(proto_id: params[:proto_id])
     @like = Like.where(proto_id: params[:proto_id], user_id: current_user.id)
